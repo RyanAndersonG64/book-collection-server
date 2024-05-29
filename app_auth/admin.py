9db1614d81c93d87e5d85a802111dfd55084bddc
RyanAndersonG64 <ryan.anderson.g64@gmail.com>
2024-05-29 10:45:18 -0400

    mmjc

--- app_auth/admin.py
+++ app_auth/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from app_auth.models import *
+
 # Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Profile, ProfileAdmin)
--- app_auth/models.py
+++ app_auth/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+# def my_decorator(bob_the_function):
+#     def wrapper():
+#         print('Before function text')
+#         bob_the_function()
+#         print('After function text')
+#     return wrapper
+
+# @my_decorator         call decorators with @ before function
+# def pizzerino():
+#     print('no pizzerino')
+
+# pizzerino()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    first_name = models.TextField()
+    last_name = models.TextField()
+
+    def __str__(self):
+        return self.user.username
